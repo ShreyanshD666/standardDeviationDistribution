2c279fa7dcfd0597ff5886c927a5a17b31eb5fb2
ShreyanshD666 <71385954+ShreyanshD666@users.noreply.github.com>
2022-06-18 21:54:39 +0530

    Update stdDeviationDistributionProject.py

--- stdDeviationDistributionProject.py
+++ stdDeviationDistributionProject.py
@@ -17,6 +17,12 @@ list_of_data_within_1_std_deviation = [result for result in data if result > fir
 list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
 list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
 
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN")) 
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1")) 
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1")) 
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2")) 
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2"))
+
 print("The mean of this data is:", mean)
 print("The median of this data is:", median)
 print("The mode of this data is:", mode)
